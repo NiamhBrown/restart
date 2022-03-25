4b58589d1f60d7e90a4194bb687a22bf467e501a
Niamh <niamh321@me.com>
2022-03-25 15:22:23 +0000

    added formatDate function

--- src/script.js
+++ src/script.js
@@ -1,16 +1,33 @@
-function displayInfo(response) {
-    console.log(response);
-    console.group(response.data.main.humidity);
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    console.log(date);   
+    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    if (hours<10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes<10) {
+        hours = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+
+}
+
+function displayInfo(response) {    
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
+    let dateElement = document.querySelector("#date");
     cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     descriptionElement.innerHTML = response.data.weather[0].description;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     humidityElement.innerHTML = response.data.main.humidity;
+    dateElement.innerHTML = formatDate (response.data.dt*1000);
 }
 
 let apiKey = "e4c991b27b566dc4b5b311b6f8d9ac5c";
