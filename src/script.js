39de5827795ae0492972b6b931ff2666dd980641
Niamh <niamh321@me.com>
2022-04-14 11:44:53 +0100

    updated icon element using API response id

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function formatDate(timestamp) {
     let date = new Date(timestamp);
-    console.log(date);   
+     
     let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
     let hours = date.getHours();
@@ -16,18 +16,26 @@ function formatDate(timestamp) {
 }
 
 function displayInfo(response) {    
+    
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
     let dateElement = document.querySelector("#date");
+  
     cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     descriptionElement.innerHTML = response.data.weather[0].description;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     humidityElement.innerHTML = response.data.main.humidity;
     dateElement.innerHTML = formatDate (response.data.dt*1000);
+
+    
+    let iconElement = document.querySelector("#icon");
+    let icon = response.data.weather[0].icon;
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "e4c991b27b566dc4b5b311b6f8d9ac5c";
