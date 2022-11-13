3cde845247d35cc2e9a6f71cb9c6697eaf1a5ecc
Niamh <niamh321@me.com>
2022-11-13 22:31:22 +0000

    sorted files

--- src/script.js
+++ src/script.js
@@ -1,83 +1,36 @@
-function formatDate(timestamp) {
-    let date = new Date(timestamp);
+function handleSubmit(){
+    var name = document.getElementById('name').value;
+    let cardTitle = document.querySelector("#cardTitle");
+    cardTitle.innerHTML = `${name}'s saving goals`;
     
-    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-
-    if (hour<10) {
-        hour = `0${hour}`;
-    }
-
-    if (minute<10) {
-        minute = `0${minute}`;
-    }
-    return `${day} ${hour}:${minute}`;
+    var reason = document.getElementById('reason').value;
+    let cardReason = document.querySelector("#cardReason");
+    cardReason.innerHTML = `${reason}`;
     
-}
-
-function displayInfo (response) {
-    let cityElement = document.querySelector("#city")
-    let descriptionElement = document.querySelector("#description");
-    let windElement = document.querySelector("#wind");
-    let temperatureElement = document.querySelector("#temperature");
-    let humidityElement = document.querySelector("#humidity");
-    let dateElement = document.querySelector("#date")
-    let iconElement = document.querySelector("#icon")
-
-    celsiusTemp = response.data.main.temp
-
-    cityElement.innerHTML = response.data.name;
-    dateElement.innerHTML = formatDate (response.data.dt*1000);
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    temperatureElement.innerHTML = Math.round(celsiusTemp);
-    humidityElement.innerHTML = Math.round(response.data.main.humidity);
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
-function search(city){
-let apiKey = "e4c991b27b566dc4b5b311b6f8d9ac5c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayInfo);
-
-}
-function handleSubmit(event){
-    event.preventDefault();
-    let inputCityElement = document.querySelector("#city-input");
-    search(inputCityElement.value);
-}
-
-function displayFahrenheitTemp(event){
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round((celsiusTemp * 9/5) + 32);
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-
-}
-
-function displayCelsiusTemp (event){
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemp);
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let celsiusTemp = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemp);
-
-search("Brighton"); 
-
+    var amount = document.getElementById('amount').value;
+    let cardAmount = document.querySelector("#cardAmount");
+    cardAmount.innerHTML = `${amount}`;
+    
+    var date = document.getElementById('date').value;
+    let cardDate = document.querySelector("#cardDate");
+    cardDate.innerHTML = `${date}`;
+    }
+    
+    //changes the background colour of the card when user clicks on coloured circles
+    
+    const cardBackground = document.querySelector(".card")
+    
+    const color1Color = document.querySelector(".color1");
+    color1Color.addEventListener("click", function () {
+    cardBackground.style.backgroundColor = "red";
+    });
+    
+    const color2Color = document.querySelector(".color2");
+    color2Color.addEventListener("click", function () {
+    cardBackground.style.backgroundColor = "grey";
+    });
+    
+    const color3Color = document.querySelector(".color3");
+    color3Color.addEventListener("click", function () {
+    cardBackground.style.backgroundColor = "blue";
+    });
