8771d8b2157a0c3bc4902e1087a31d589b06878a
Niamh <niamh321@me.com>
2022-11-13 20:19:31 +0000

    basic function working on submit

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 
-function displayName(){
+function handleSubmit(){
 var name = document.getElementById('name').value;
 let cardTitle = document.querySelector("#cardTitle");
 cardTitle.innerHTML = `${name}'s saving goals`;
+
+var reason = document.getElementById('reason').value;
+let cardReason = document.querySelector("#cardReason");
+cardReason.innerHTML = `${reason}`;
+
+var amount = document.getElementById('amount').value;
+let cardAmount = document.querySelector("#cardAmount");
+cardAmount.innerHTML = `${amount}`;
+
+var date = document.getElementById('date').value;
+let cardDate = document.querySelector("#cardDate");
+cardDate.innerHTML = `${date}`;
 }
 
