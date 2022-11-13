13e966c721222b5634700f7690644f332748d4a6
Niamh <niamh321@me.com>
2022-11-13 20:02:39 +0000

    got first field submit to work

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("")
+
+
+function displayName(){
+var name = document.getElementById('name').value;
+let cardTitle = document.querySelector("#cardTitle");
+cardTitle.innerHTML = `${name}'s saving goals`;
+}
+
